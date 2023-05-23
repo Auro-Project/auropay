335b2f868ab939ab76c5f3d5c3d7b74b912ad4b5
snehajhaveri01 <jhaverisneha3@gmail.com>
2023-05-23 19:09:26 +0530

    test Text

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Payment service app with blockchain
 
 All setup on Jira Management System
+
+#sbranch 
+
+can you do ?
+
+tell me your github email add
+jhaverisneha3@gmail.com
+snehajhaveri01
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:auropay/view/pages/Send/ProgressScreen.dart';
-import 'package:auropay/view/pages/Send/SendScreen.dart';
-import 'package:auropay/view/pages/Send/SuccessScreen.dart';
 import 'package:auropay/view/pages/onBoardScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -39,9 +36,6 @@ class MyApp extends StatelessWidget {
         '/transactions': (context) => const TransactionScreen(),
         '/analytics': (context) => const AnalyticsScreen(),
         '/more': (context) => const MoreScreen(),
-        '/send': (context) => const SendScreen(),
-        '/progress': (context) => const ProgressScreen(),
-        '/success': (context) => const SuccessScreen(),
       },
     );
   }
