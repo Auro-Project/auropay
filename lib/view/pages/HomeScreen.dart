7d304113d8554bec785e349e1f476c93ca78ada1
snehajhaveri01 <jhaverisneha3@gmail.com>
2023-06-08 03:11:53 +0530

    Updated Login Screens and HomeScreen

--- lib/view/pages/HomeScreen.dart
+++ lib/view/pages/HomeScreen.dart
@@ -248,7 +248,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return SizedBox(
-      width: 90,
+      width: 140,
       child: Column(
         children: [
           ElevatedButton(
@@ -257,10 +257,9 @@ class _HomeScreenState extends State<HomeScreen> {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(15),
               ),
-                minimumSize: const Size(200, 50)
             ),
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 17),
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 17),
               child: SvgPicture.asset(
                 icon,
                 height: 30,
