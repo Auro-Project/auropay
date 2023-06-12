d095fe2884d44c97f4aefa7067ab82e1de040d80
snehajhaveri01 <jhaverisneha3@gmail.com>
2023-06-12 14:31:21 +0530

    commit1

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 Payment service app with blockchain
 
 All setup on Jira Management System
+
+1. mpin - responsiveness
+2. bg color - profile
+3. profile buttons 
+      1. no need of analytics there 
+      2. and notifications is already there on homescreen
+      3. settings and logout will be on the list 
+      4. list buttons aren't working ?
+      5. profile - edit profile ? 
+4. transaction page - withdrawal & paid , what will go there ?
+        1. align - all, withdrawal & paid
+5. analytics - will add pie chart on top and then display of daily,
+   monthly and yearly expenses 
+6. top up screen - idk what's the point of the rounded rectangle and amount 
+        buttons color should be lighter , don't want too much color on one page
+7. need work on withdrawal page 
+8. in qrscan , i was thinking to add those transactions screen for 
+   processing and completion  , need a page for adding how much amount to pay
+9. if we go back from profile, analytics and transaction screen, 
+    it leads to mpin page, change it to homescreen
--- lib/view/pages/LoginScreens/SignedUserScreen.dart
+++ lib/view/pages/LoginScreens/SignedUserScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:auropay/view/Theme/appColors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -56,7 +55,8 @@ class _SignedUserScreenState extends State<SignedUserScreen> {
   }
 
   void checkPasscode(int index, String value) async {
-    if (index == 3 && value.isNotEmpty) { // If it's the last field and it's filled
+    if (index == 3 && value.isNotEmpty) {
+      // If it's the last field and it's filled
       if (await confirmPasscode()) {
         Navigator.pushNamed(context, '/home');
       } else {
@@ -73,142 +73,151 @@ class _SignedUserScreenState extends State<SignedUserScreen> {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body: Container(
-        // decoration: const BoxDecoration(
-        //   gradient: LinearGradient(
-        //     colors: [
-        //       Color(0xFFDBDEF3),
-        //       Color(0xFF99ADEF),
-        //     ],
-        //     begin: Alignment.topCenter,
-        //     end: Alignment.bottomCenter,
-        //   ),
-        // ),
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/images/shapes/gradHM.png',
-              width: MediaQuery.of(context).size.width,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                const SizedBox(height: 40),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 36),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: Container(
+                  // decoration: const BoxDecoration(
+                  //   gradient: LinearGradient(
+                  //     colors: [
+                  //       Color(0xFFDBDEF3),
+                  //       Color(0xFF99ADEF),
+                  //     ],
+                  //     begin: Alignment.topCenter,
+                  //     end: Alignment.bottomCenter,
+                  //   ),
+                  // ),
+                  child: Stack(
                     children: [
-                      // SvgPicture.asset(
-                      //   'assets/images/icons/Logo.svg',
-                      //   width: 100,
-                      //   height: 100,
-                      //   color: Colors.white.withOpacity(0.8),
-                      // ),
-
                       Image.asset(
-                        'assets/images/pscode.png',
-                        width: 200,
-                        height: 200,
+                        'assets/images/shapes/gradHM.png',
+                        width: MediaQuery.of(context).size.width,
                       ),
-                      const SizedBox(height: 30),
-                      Text(
-                        'Hello, Naresh!',
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'SF Pro Display',
-                          color: AppColors.textColor,
-
-                        ),
-                      ),
-                      const SizedBox(height: 60),
-                      Text(
-                        'Enter your MPIN',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal,
-                          fontFamily: 'SF Pro Display',
-                          color: AppColors.textColor,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          for (int i = 0; i < 4; i++)
-                            Container(
-                              width: 60,
-                              height: 60,
-                              margin: const EdgeInsets.symmetric(horizontal: 10),
-                              decoration: border(context, borderRadius: 15),
-                              child: TextField(
-                                controller: passcodeControllers[i],
-                                keyboardType: TextInputType.number,
-                                maxLength: 1,
-                                obscureText: true,
-                                style: TextStyle(
-                                  color: AppColors.textColor,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
+                          const SizedBox(height: 40),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: constraints.maxWidth * 0.1),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                // SvgPicture.asset(
+                                //   'assets/images/icons/Logo.svg',
+                                //   width: 100,
+                                //   height: 100,
+                                //   color: Colors.white.withOpacity(0.8),
+                                // ),
+                                Image.asset(
+                                  'assets/images/pscode.png',
+                                  width: constraints.maxWidth * 0.5,
+                                  height: constraints.maxWidth * 0.5,
                                 ),
-                                textAlign: TextAlign.center,
-                                onChanged: (value) {
-                                  checkPasscode(i, value);
-                                  if (value.isNotEmpty && i < 3) {
-                                    focusNextPasscodeField();
-                                  }
-                                },
-                                decoration: const InputDecoration(
-                                  counterText: '',
-                                  border: InputBorder.none,
+                                const SizedBox(height: 30),
+                                const Text(
+                                  'Hello, Naresh!',
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'SF Pro Display',
+                                    color: AppColors.textColor,
+                                  ),
                                 ),
-                              ),
+                                const SizedBox(height: 60),
+                                const Text(
+                                  'Enter your MPIN',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal,
+                                    fontFamily: 'SF Pro Display',
+                                    color: AppColors.textColor,
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    for (int i = 0; i < 4; i++)
+                                      Container(
+                                        width: constraints.maxWidth * 0.12,
+                                        height: constraints.maxWidth * 0.12,
+                                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                                        decoration: border(context, borderRadius: 15),
+                                        child: TextField(
+                                          controller: passcodeControllers[i],
+                                          keyboardType: TextInputType.number,
+                                          maxLength: 1,
+                                          obscureText: true,
+                                          style: const TextStyle(
+                                            color: AppColors.textColor,
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                          onChanged: (value) {
+                                            checkPasscode(i, value);
+                                            if (value.isNotEmpty && i < 3) {
+                                              focusNextPasscodeField();
+                                            }
+                                          },
+                                          decoration: const InputDecoration(
+                                            counterText: '',
+                                            border: InputBorder.none,
+                                          ),
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                                const SizedBox(height: 20),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Forgot MPIN?',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal,
+                                      fontFamily: 'SF Pro Display',
+                                      color: AppColors.accent1,
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
+                          ),
+                          const SizedBox(height: 50),
+                          IconButton(
+                            onPressed: () => Navigator.pushNamed(context, '/home'),
+                            icon: SvgPicture.asset(
+                              'assets/images/icons/faceid.svg',
+                              width: constraints.maxWidth * 0.1,
+                              color: AppColors.textColor,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Use Face ID',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: 'SF Pro Display',
+                              color: AppColors.textColor,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          const Expanded(child: SizedBox()),
                         ],
                       ),
-                      const SizedBox(height: 20),
-                      TextButton(
-                          onPressed: (){},
-                          child: Text('Forgot MPIN?',style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal,
-                            fontFamily: 'SF Pro Display',
-                            color: AppColors.accent1,
-                          ),)
-                      )
                     ],
                   ),
                 ),
-                const SizedBox(height: 50),
-                IconButton(
-                  onPressed: () => Navigator.pushNamed(context, '/home'),
-                  icon: SvgPicture.asset(
-                    'assets/images/icons/faceid.svg',
-                    width: 40,
-                    color: AppColors.textColor,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Use Face ID',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                    fontFamily: 'SF Pro Display',
-                    color: AppColors.textColor,
-                  ),
-                ),
-                const SizedBox(height: 16),
-              ],
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
-
 }
-
