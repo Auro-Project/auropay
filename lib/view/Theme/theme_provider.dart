7f2882f6714954e359f5913ada9dc54b0934e256
nj4444 <114717738+nj4444@users.noreply.github.com>
2023-06-29 02:34:36 +0530

    minor updates for theme state

--- lib/view/Theme/theme_provider.dart
+++ lib/view/Theme/theme_provider.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:auropay/view/Theme/appColors.dart';
-
-enum ThemeModeType { light, dark }
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
   final ThemeData _lightTheme = ThemeData.light();
   final ThemeData _darkTheme = ThemeData.dark();
 
   ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeProvider() {
+    _loadThemeMode();
+  }
 
   ThemeData getTheme() {
     return _themeMode == ThemeMode.light ? _lightTheme : _darkTheme;
@@ -19,9 +21,19 @@ class ThemeProvider with ChangeNotifier {
 
   void toggleTheme() {
     _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    _saveThemeMode(_themeMode);
     notifyListeners();
   }
+
+  void _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = prefs.getBool('isDark') ?? false;
+    _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+
+  void _saveThemeMode(ThemeMode themeMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDark', themeMode == ThemeMode.dark);
+  }
 }
-
-
-
